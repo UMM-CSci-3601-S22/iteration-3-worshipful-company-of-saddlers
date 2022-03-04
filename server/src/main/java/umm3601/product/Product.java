cf3d14cf49eb81e5c977d5531a60b8b736510af9
Daniel Albright <albri118@morris.umn.edu>
2022-03-04 17:21:15 -0600

    Import built in java library with Image type

--- server/src/main/java/umm3601/product/Product.java
+++ server/src/main/java/umm3601/product/Product.java
@@ -1,11 +1,14 @@
 package umm3601.product;
 
+import java.awt.Image;
+
 public class Product {
   public String _id;
   public String product_name;
   public String description;
   public String brand;
-  public String category; // bakery, produce, meat, dairy, frozen foods, canned goods, drinks
+  public String category; // bakery, produce, meat, dairy, frozen foods, canned goods, drinks,
+                          // miscellaneous
   public String store;
   public String location;
   public String notes;
