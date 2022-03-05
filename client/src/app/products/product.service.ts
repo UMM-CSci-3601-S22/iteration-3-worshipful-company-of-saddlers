e5043a4c8b495edc8aa5bf3f0ccdc08c8ea65e2c
Daniel Albright <albri118@morris.umn.edu>
2022-03-05 15:35:18 -0600

    Create getProduct(s) methods Fixes #46

--- client/src/app/products/product.service.ts
+++ client/src/app/products/product.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Product, ProductCategory } from './product';
+import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProductService {
+  readonly productUrl: string = environment.apiUrl + 'products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getProducts(filters?: { category?: ProductCategory; store?: string }): Observable<Product[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.category) {
+        httpParams = httpParams.set('category', filters.category);
+      }
+      if (filters.store) {
+        httpParams = httpParams.set('store', filters.store);
+      }
+    }
+    return this.httpClient.get<Product[]>(this.productUrl, {
+      params: httpParams,
+    });
+  }
+
+  getProductById(id: string): Observable<Product> {
+    return this.httpClient.get<Product>(this.productUrl + '/' + id);
+  }
+
 }
--- client/src/app/products/product.ts
+++ client/src/app/products/product.ts
@@ -1,16 +1,16 @@
 export interface Product {
   _id: string;
-  productName: string;
-  description?: string;
-  brand: string;
-  category: ProductCategory;
-  store: string;
-  location?: string;
-  notes?: string;
-  tags?: string[];
-  lifespan?: number;
-  threshold?: number;
-  image?: File;
+  productName: string; // Client filter
+  description?: string; // Client filter
+  brand: string; // client filter
+  category: ProductCategory; // server filter
+  store: string; // server filter
+  location?: string; // client filter
+  notes?: string; // server filter
+  tags?: string[]; // client filter
+  lifespan?: number; // server filter
+  threshold?: number; // server filter
+  image?: File; // server filter
 }
 
 export type ProductCategory = 'bakery' | 'produce' | 'meat' | 'diary' | 'frozen foods' | 'canned goods' | 'drinks' | 'miscellaneous';
