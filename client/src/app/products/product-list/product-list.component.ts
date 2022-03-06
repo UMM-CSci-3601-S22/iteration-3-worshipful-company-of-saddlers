86e020be8c134c2f6c71fb5e1cf91f1a49267069
Daniel Albright <albri118@morris.umn.edu>
2022-03-05 19:42:24 -0600

    Setup product-list logic methods

--- client/src/app/products/product-list/product-list.component.ts
+++ client/src/app/products/product-list/product-list.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Product, ProductCategory } from '../product';
+import { ProductService } from '../product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-product-list',
+  selector: 'app-product-list-component',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
+  providers: []
 })
-export class ProductListComponent implements OnInit {
 
-  constructor() { }
+export class ProductListComponent implements OnInit, OnDestroy {
+  public serverFilteredProducts: Product[];
+  public filteredProducts: Product[];
+
+  public name: string;
+  public productBrand: string;
+  public productCategory: ProductCategory;
+  public productStore: string;
+  getProductsSub: Subscription;
+
+  constructor(private productService: ProductService) { }
+
+  getProductsFromServer(): void {
+    this.unsub();
+    this.getProductsSub = this.productService.getProducts({
+      category: this.productCategory,
+      store: this.productStore
+    }).subscribe(returnedProducts => {
+      this.serverFilteredProducts = returnedProducts;
+      this.updateFilter();
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  public updateFilter(): void {
+    this.filteredProducts = this.productService.filterProducts(
+      this.serverFilteredProducts, { productName: this.name, brand: this.productBrand });
+  }
 
   ngOnInit(): void {
+    this.getProductsFromServer();
+  }
+
+  ngOnDestroy(): void {
+    this.unsub();
+  }
+
+  unsub(): void {
+    if (this.getProductsSub) {
+      this.getProductsSub.unsubscribe();
+    }
   }
 
 }
