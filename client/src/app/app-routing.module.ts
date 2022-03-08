efcc3b9c564dbc5ecaf7fa25a2eb57bd7250383a
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2022-03-07 21:22:06 -0600

    Remove users from router and side bar

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,15 +7,13 @@ import { AddUserComponent } from './users/add-user.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { AddProductComponent } from './products/add-product/add-product.component';
 import { SingleProductPageComponent } from './products/single-product-page/single-product-page.component';
+import { PantryProductsListComponent } from './pantry/pantry-products-list/pantry-products-list.component';
 
 // Note that the 'users/new' route needs to come before 'users/:id'.
 // If 'users/:id' came first, it would accidentally catch requests to
 // 'users/new'; the router would just think that the string 'new' is a user ID.
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'users', component: UserListComponent},
-  {path: 'users/new', component: AddUserComponent},
-  {path: 'users/:id', component: UserProfileComponent},
+  {path: '', component: PantryProductsListComponent},
   {path: 'products', component: ProductListComponent},
   {path: 'products/new', component: AddProductComponent},
   {path: 'products/:id', component: SingleProductPageComponent}
