2e0a62c0595ad2aaccfd415518050962cb0676d2
Cole Maxwell <cole.n.maxwell99@gmail.com>
2022-03-08 16:18:29 -0600

    Correct lgtm unused import notice

--- client/src/app/pantry/pantry.service.ts
+++ client/src/app/pantry/pantry.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Product, ProductCategory } from '../products/product';
+import { Product } from '../products/product';
 
 @Injectable()
 export class PantryService {
