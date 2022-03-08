f3de8334517ed58590cd6a09efbc858b6148289b
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2022-03-08 13:31:37 -0600

    Add pantry service

--- client/src/app/pantry/pantry.service.spec.ts
+++ client/src/app/pantry/pantry.service.spec.ts
@@ -1,16 +1,91 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Product } from '../products/product';
 
 import { PantryService } from './pantry.service';
 
 describe('PantryService', () => {
-  let service: PantryService;
+
+  const testPantryProducts: Product[] = [
+    {
+      _id: 'banana_id',
+      product_name: 'banana',
+      description: '',
+      brand: 'Dole',
+      category: 'produce',
+      store: 'Walmart',
+      location: '',
+      notes: '',
+      tags: [],
+      lifespan: 0,
+      threshold: 0,
+      image: ''
+    },
+    {
+      _id: 'milk_id',
+      product_name: 'Whole Milk',
+      description: '',
+      brand: 'Land O Lakes',
+      category: 'dairy',
+      store: 'SuperValu',
+      location: '',
+      notes: '',
+      tags: [],
+      lifespan: 0,
+      threshold: 0,
+      image: ''
+    },
+    {
+      _id: 'bread_id',
+      product_name: 'Wheat Bread',
+      description: '',
+      brand: 'Country Hearth',
+      category: 'bakery',
+      store: 'Walmart',
+      location: '',
+      notes: '',
+      tags: [],
+      lifespan: 0,
+      threshold: 0,
+      image: ''
+    }
+  ];
+  let pantryService: PantryService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PantryService);
+    // Set up the mock handling of the HTTP requests
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    // Construct an instance of the service with the mock
+    // HTTP client.
+    pantryService = new PantryService(httpClient);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
+
+  it('getPantryItems() calls api/pantry', () => {
+    // Assert that the products we get from this call to getProducts()
+    // should be our set of test products. Because we're subscribing
+    // to the result of getProducts(), this won't actually get
+    // checked until the mocked HTTP request 'returns' a response.
+    // This happens when we call req.flush(testProducts) a few lines
+    // down.
+    pantryService.getPantryItems().subscribe(
+      products => expect(products).toBe(testPantryProducts)
+    );
+
+  });
+
+
+
 });
--- client/src/app/pantry/pantry.service.ts
+++ client/src/app/pantry/pantry.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Product, ProductCategory } from '../products/product';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class PantryService {
+  readonly pantryUrl: string = environment.apiUrl + 'pantry';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getPantryItems(filters?: { category?: ProductCategory; store?: string }): Observable<Product[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.category) {
+        httpParams = httpParams.set('category', filters.category);
+      }
+      if (filters.store) {
+        httpParams = httpParams.set('store', filters.store);
+      }
+    }
+    return this.httpClient.get<Product[]>(this.pantryUrl, {
+      params: httpParams,
+    });
+  }
+
+
 }
