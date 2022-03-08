6d3812fa09fb1a02e54625997fa2370c01238abb
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2022-03-08 13:31:52 -0600

    Start pantry products list

--- client/src/app/pantry/pantry-products-list/pantry-products-list.component.ts
+++ client/src/app/pantry/pantry-products-list/pantry-products-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
+import { Product, ProductCategory } from 'src/app/products/product';
+import { PantryService } from '../pantry.service';
 
 @Component({
   selector: 'app-pantry-products-list',
@@ -6,10 +10,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pantry-products-list.component.scss']
 })
 export class PantryProductsListComponent implements OnInit {
+  // Unfiltered product list
+  public allProducts: Product[];
 
-  constructor() { }
+  public name: string;
+  public productBrand: string;
+  public productCategory: ProductCategory;
+  public productStore: string;
+  public productLimit: number;
+  getProductsSub: Subscription;
+  getUnfilteredProductsSub: Subscription;
 
-  ngOnInit(): void {
+
+
+  /**
+   * This constructor injects both an instance of `PantryService`
+   * and an instance of `MatSnackBar` into this component.
+   *
+   * @param pantryService the `PantryService` used to get products in the pantry
+   * @param snackBar the `MatSnackBar` used to display feedback
+   */
+  constructor(private pantryService: PantryService, private snackBar: MatSnackBar) {
+    // Nothing here – everything is in the injection parameters.
   }
+
+  /*
+  * Get the products in the pantry from the server,
+  */
+  getPantryItemsFromServer() {
+    this.pantryService.getPantryItems({
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+    }).subscribe(returnedPantryProducts => {
+
+      this.allProducts = returnedPantryProducts;
+    }, err => {
+      // If there was an error getting the users, log
+      // the problem and display a message.
+      console.error('We couldn\'t get the list of todos; the server might be down');
+      this.snackBar.open(
+        'Problem contacting the server – try again',
+        'OK',
+        // The message will disappear after 3 seconds.
+        { duration: 3000 });
+    });
+  }
+
+  /*
+  * Starts an asynchronous operation to update the users list
+  */
+  ngOnInit(): void {
+    this.getPantryItemsFromServer();
+  }
+
 
 }
