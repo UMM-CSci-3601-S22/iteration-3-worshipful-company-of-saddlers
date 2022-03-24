401e63ba44a1eb816862c3f18cefd0867589a63f
krug0102 <krug0102@morris.umn.edu>
2022-03-24 16:11:26 -0500

    Start implementing `add-pantry-item-component`
    Co-authored-by: nikfbail <bail0301@morris.umn.edu>

--- client/cypress/support/add-pantry-item.po.ts
+++ client/cypress/support/add-pantry-item.po.ts
@@ -1,7 +1,21 @@
-import { PantryService } from 'src/app/pantry/pantry.service';
+import { PantryItem } from '../../src/app/pantry/pantryItem';
 
 export class AddItemPage {
   navigateTo() {
     return cy.visit('/pantry/new');
+  }
+
+  getTitle() {
+    return cy.get('.add-pantry-item-title');
+  }
+
+  addPantryItemButton() {
+    return cy.get('[data-test=confirmAddItemButton]');
+  }
+
+
+
+  addPantryItem(newItem: PantryItem) {
+    return this.addPantryItemButton().click();
   }
 }
--- client/src/app/pantry/add-pantry-item/add-pantry-item.component.ts
+++ client/src/app/pantry/add-pantry-item/add-pantry-item.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Product } from 'src/app/products/product';
+import { ProductService } from 'src/app/products/product.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-pantry-item',
   templateUrl: './add-pantry-item.component.html',
   styleUrls: ['./add-pantry-item.component.scss']
 })
-export class AddPantryItemComponent implements OnInit {
+export class AddPantryItemComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public allProducts: Product[];
 
+  getUnfilteredProductsSub: Subscription;
+
+  constructor(private productService: ProductService, private snackBar: MatSnackBar) { }
+
+  // This needs to be able to get all of the products from the product database,
+  // so that we can search for a product
   ngOnInit(): void {
+    this.getUnfilteredProducts();
   }
 
+  ngOnDestroy(): void {
+    this.unsubUnfiltered();
+  }
+
+  getUnfilteredProducts(): void {
+    this.unsubUnfiltered();
+    this.getUnfilteredProductsSub = this.productService.getProducts().subscribe(returnedProducts => {
+      this.allProducts = returnedProducts;
+    });
+  }
+
+  unsubUnfiltered(): void {
+    if (this.getUnfilteredProductsSub) {
+      this.getUnfilteredProductsSub.unsubscribe();
+    }
+  }
+
+  filterProducts(product_name: string): Product[] {
+    return this.productService.filterProducts(this.allProducts, {product_name});
+  }
 }
