0d3172207cac734ef28a08188f9a20361a39ce22
Nik Bailey <bail0301@morris.umn.edu>
2022-03-28 15:02:23 -0500

    Add functionality for getting all products and filtering by name
    Co-authored-by: krug0102 <krug0102@morris.umn.edu>

--- client/src/app/pantry/pantry-products-list/pantry-products-list.component.ts
+++ client/src/app/pantry/pantry-products-list/pantry-products-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Product, ProductCategory } from 'src/app/products/product';
+import { ProductService } from 'src/app/products/product.service';
 import { PantryService } from '../pantry.service';
 
 @Component({
@@ -9,16 +11,19 @@ import { PantryService } from '../pantry.service';
   templateUrl: './pantry-products-list.component.html',
   styleUrls: ['./pantry-products-list.component.scss']
 })
-export class PantryProductsListComponent implements OnInit {
+export class PantryProductsListComponent implements OnInit, OnDestroy {
   // Unfiltered product list
   public allProducts: Product[];
+  public pantryProducts: Product[];
+  public filteredProducts: Product[];
 
   public name: string;
   public productBrand: string;
   public productCategory: ProductCategory;
   public productStore: string;
-  public productLimit: number;
-  getProductsSub: Subscription;
+
+  public activeFilters: boolean;
+
   getUnfilteredProductsSub: Subscription;
 
 
@@ -30,7 +35,8 @@ export class PantryProductsListComponent implements OnInit {
    * @param pantryService the `PantryService` used to get products in the pantry
    * @param snackBar the `MatSnackBar` used to display feedback
    */
-  constructor(private pantryService: PantryService, private snackBar: MatSnackBar) {
+  constructor(private pantryService: PantryService, private productService: ProductService,
+     private snackBar: MatSnackBar) {
     // Nothing here – everything is in the injection parameters.
   }
 
@@ -40,17 +46,36 @@ export class PantryProductsListComponent implements OnInit {
   getPantryItemsFromServer() {
     this.pantryService.getPantryItems().subscribe(returnedPantryProducts => {
 
-      this.allProducts = returnedPantryProducts;
+      this.pantryProducts = returnedPantryProducts;
     }, err => {
       // If there was an error getting the users, log
       // the problem and display a message.
-      console.error('We couldn\'t get the list of todos; the server might be down');
+      console.error('We couldn\'t get the list of products in your pantry; the server might be down');
       this.snackBar.open(
-        'Problem contacting the server – try again',
+        'Problem contacting the server - try again',
         'OK',
         // The message will disappear after 3 seconds.
         { duration: 3000 });
     });
+  }
+
+  getUnfilteredProducts(): void {
+    this.unsubUnfiltered();
+    this.getUnfilteredProductsSub = this.productService.getProducts().subscribe(returnedProducts => {
+      this.allProducts = returnedProducts;
+    });
+  }
+
+  updateFilter(): void {
+    this.filteredProducts = this.productService.filterProducts(
+      this.allProducts, { product_name: this.name }
+    );
+    if( this.name ) {
+      this.activeFilters = true;
+    }
+    else {
+      this.activeFilters = false;
+    }
   }
 
   /*
@@ -58,7 +83,16 @@ export class PantryProductsListComponent implements OnInit {
   */
   ngOnInit(): void {
     this.getPantryItemsFromServer();
+    this.getUnfilteredProducts();
   }
 
+  ngOnDestroy(): void {
+    this.unsubUnfiltered();
+  }
 
+  unsubUnfiltered(): void {
+    if (this.getUnfilteredProductsSub) {
+      this.getUnfilteredProductsSub.unsubscribe();
+    }
+  }
 }
