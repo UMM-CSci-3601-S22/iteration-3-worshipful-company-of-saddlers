aae3c3363837ab5e13788dc2018df42ed62db765
Shua <enge0479@morris.umn.edu>
2022-03-30 10:40:02 -0500

    Clean up product-list cypress tests
    There were too many "tests" and although the assertions were useful, grouping them separately was not. I made the testing for opening and closing extension panels appear as one test with many assertions, so that when tests for delete are added, there will not be 5 tests for each category. That would result in 50 tests for the product dropdowns alone.

--- client/cypress/integration/product-list.spec.ts
+++ client/cypress/integration/product-list.spec.ts
@@ -126,200 +126,140 @@ describe('Product list', () => {
   });
 
   describe('Produce product list works', () => {
-
-    it('The produce products should be invisible by default', () => {
+    it('The produce products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the produce products should be hidden
       page.getProduceProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The produce products panel should show', () => {
       page.getProduceProductDropdown()
         .should('exist');
-    });
-
-    it('The produce product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the produce products should not be hidden
       page.getProduceProductDropdown().click();
       page.getProduceProductListItems().should('be.visible');
     });
   });
 
   describe('Bakery product list works', () => {
-
-    it('The bakery products should be invisible by default', () => {
+    it('The bakery products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the bakery products should be hidden
       page.getBakeryProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The bakery products panel should show', () => {
       page.getBakeryProductDropdown()
         .should('exist');
-    });
-
-    it('The bakery product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the bakery products should not be hidden
       page.getBakeryProductDropdown().click();
       page.getBakeryProductListItems().should('be.visible');
     });
   });
 
   describe('Meat product list works', () => {
-
-    it('The meat products should be invisible by default', () => {
+    it('The meat products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the meat products should be hidden
       page.getMeatProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The meat products panel should show', () => {
       page.getMeatProductDropdown()
         .should('exist');
-    });
-
-    it('The meat product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the meat products should not be hidden
       page.getMeatProductDropdown().click();
       page.getMeatProductListItems().should('be.visible');
     });
   });
 
   describe('Dairy product list works', () => {
-
-    it('The dairy products should be invisible by default', () => {
+    it('The dairy products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the dairy products should be hidden
       page.getDairyProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The dairy products panel should show', () => {
       page.getDairyProductDropdown()
         .should('exist');
-    });
-
-    it('The dairy product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the dairy products should not be hidden
       page.getDairyProductDropdown().click();
       page.getDairyProductListItems().should('be.visible');
     });
   });
 
   describe('Drinks product list works', () => {
-
-    it('The drink products should be invisible by default', () => {
+    it('The drink products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the drink products should be hidden
       page.getDrinkProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The drink products panel should show', () => {
       page.getDrinkProductDropdown()
         .should('exist');
-    });
-
-    it('The drink product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the drink products should not be hidden
       page.getDrinkProductDropdown().click();
       page.getDrinkProductListItems().should('be.visible');
     });
   });
 
   describe('Frozen food product list works', () => {
-
-    it('The frozen food products should be invisible by default', () => {
+    it('The frozen food products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the frozen food products should be hidden
       page.getFrozenProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The frozen food products panel should show', () => {
       page.getFrozenProductDropdown()
         .should('exist');
-    });
-
-    it('The frozen food product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the frozen food products should not be hidden
       page.getFrozenProductDropdown().click();
       page.getFrozenProductListItems().should('be.visible');
     });
   });
 
   describe('Canned goods product list works', () => {
-
-    it('The canned goods products should be invisible by default', () => {
+    it('The canned goods products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the canned goods products should be hidden
       page.getCannedProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The canned goods products panel should show', () => {
       page.getCannedProductDropdown()
         .should('exist');
-    });
-
-    it('The canned goods product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the canned goods products should not be hidden
       page.getCannedProductDropdown().click();
       page.getCannedProductListItems().should('be.visible');
     });
   });
 
   describe('General product list works', () => {
-
-    it('The general products should be invisible by default', () => {
+    it('The general products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the general products should be hidden
       page.getGeneralProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The general products panel should show', () => {
       page.getGeneralProductDropdown()
         .should('exist');
-    });
-
-    it('The general product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the general products should not be hidden
       page.getGeneralProductDropdown().click();
       page.getGeneralProductListItems().should('be.visible');
     });
   });
 
   describe('Seasonal product list works', () => {
-
-    it('The seasonal products should be invisible by default', () => {
+    it('The seasonal products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the seasonal products should be hidden
       page.getSeasonalProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The seasonal products panel should show', () => {
       page.getSeasonalProductDropdown()
         .should('exist');
-    });
-
-    it('The seasonal product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the seasonal products should not be hidden
       page.getSeasonalProductDropdown().click();
       page.getSeasonalProductListItems().should('be.visible');
     });
   });
 
   describe('Miscellaneous product list works', () => {
-
-    it('The miscellaneous products should be invisible by default', () => {
+    it('The miscellaneous products should appear and disappear when the panel is clicked', () => {
       // Before clicking on the button, the miscellaneous products should be hidden
       page.getMiscellaneousProductListItems()
         .should('be.hidden')
         .and('not.be.visible');
-    });
-
-    it('The miscellaneous products panel should show', () => {
       page.getMiscellaneousProductDropdown()
         .should('exist');
-    });
-
-    it('The miscellaneous product should appear and disappear when the panel is clicked', () => {
+      // After clicking the dropdown panel, the miscellaneous products should not be hidden
       page.getMiscellaneousProductDropdown().click();
       page.getMiscellaneousProductListItems().should('be.visible');
     });
