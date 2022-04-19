6f8f200e7f6e8914fd93add58f4d7c8fa82b2f66
Nic McPhee <mcphee@morris.umn.edu>
2022-04-18 19:27:32 -0500

    Convert categories to field names
    There are a lot places where you have duplicate code when you could be looping over categories.
    
    This changes how `ProductCategory` is defined so that we can enumerate over categories, as suggested here: https://stackoverflow.com/a/64174790
    
    The new function `categoryCamelCase()` converts categories into a camel case form with `Items` appended at the end, i.e., `'pet supplies'` is mapped to `petSuppliesItems`.
    
    I *think* you'd eventually want to get rid of this function, perhaps by either just doing everything with the camel case version, or by having an enum that maps the camel case version to the version with the spaces.
    
    I'm also not sure it you want to be adding the suffix `Items` here in this function. It was needed everywhere I called `categoryCamelCase` so I put it here, but I feel like there might be places where you want the camel case version but don't want/need 'Items' at the end. If that's true, then we probably want to move adding 'Items' off to where it's actually needed.

--- client/src/app/products/product.ts
+++ client/src/app/products/product.ts
@@ -14,7 +14,36 @@ export interface Product {
   image?: string; // server filter
 }
 
-// eslint-disable-next-line max-len
-export type ProductCategory = 'baked goods' | 'produce' | 'meat' | 'dairy' | 'frozen foods' | 'baking supplies'
-| 'beverages' | 'cleaning products' | 'miscellaneous' | 'deli'
-|'herbs and spices' | 'paper products' | 'pet supplies' | 'staples' | 'toiletries';
+export const categories =
+  ['baked goods', 'produce', 'meat', 'dairy', 'frozen foods',
+   'baking supplies', 'beverages', 'cleaning products', 'miscellaneous', 'deli',
+   'herbs and spices', 'paper products', 'pet supplies', 'staples', 'toiletries'] as const;
+export type ProductCategory = typeof categories[number];
+
+export const categoryCamelCase = (category: ProductCategory) => {
+  // This is essentially a hack to handle all the categories with spaces
+  // in them. One could have done that programmatically, but I didn't want
+  // to add that computational overhead given how often this will be called.
+  // I suspect there's a better solution, TBH.
+  const categoryMap = {
+    'baked goods': 'bakedGoods',
+    'frozen foods': 'frozenFoods',
+    'baking supplies': 'bakingSupplies',
+    'cleaning products': 'cleaningProducts',
+    'herbs and spices': 'herbsAndSpices',
+    'paper products': 'paperProducts',
+    'pet supplies': 'petSupplies'
+  };
+  let categoryPrefix: string;
+  if (categoryMap[category]) {
+    categoryPrefix = categoryMap[category];
+  } else {
+    categoryPrefix = category;
+  }
+  // I don't know if we want/need to add 'Items' here. It was needed everywhere
+  // I called this from, but I can imagine that if you generalized this idea
+  // you might have uses that don't want 'Items' at the end. If so, then you
+  // want to move the addition of 'Items' to the places where it's necessary
+  // instead of having it here.
+  return categoryPrefix + 'Items';
+};
